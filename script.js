6d0f581ef6a3359da26d505f1b58530ed91da0c0
Tatyna Kubatova <tatynakubatova74@gmail.com>
2022-02-12 23:57:09 +0600

    Create drawing app

---
+++ script.js
@@ -0,0 +1,56 @@
+const canvas = document.getElementById("canvas");
+let color = document.querySelector('#color');
+let sizes = document.querySelector('#sizes');
+let onload = document.querySelector('#onload');
+
+//Default pen size
+ctx.lineWidth = 5;
+
+//Change color
+color.addEventListener('change', event => {
+  ctx.strokeStyle = color.value;
+});
+//Change size
+sizes.addEventListener('change', event => {
+  ctx.lineWidth = sizes.value;
+});
+//Delete all
+onload.addEventListener('click', event => {
+  window.location.reload();
+});
+
+//Canva's height and width
+canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
+
+//Canva context(pen)
+let ctx = canvas.getContext("2d");
+
+let prevX = null;
+let prevY = null;
+
+let draw = false;
+
+// Set draw to true when mouse is pressed
+window.addEventListener("mousedown", (event) => draw = true);
+// Set draw to false when mouse is released
+window.addEventListener("mouseup", (event) => draw = false);
+
+window.addEventListener("mousemove", (event) => {
+  // if draw is false then we won't draw
+  if (prevX == null || prevY == null || !draw) {
+    prevX = event.x;
+    prevY = event.y;
+    return;
+  };
+  let currentX = event.x;
+  let currentY = event.y;
+
+  ctx.beginPath();
+  ctx.moveTo(prevX, prevY);
+  ctx.lineTo(currentX, currentY);
+  ctx.stroke();
+
+  prevX = currentX;
+  prevY = currentY;
+});
